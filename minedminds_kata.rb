cdb828f0c71863a313f979aa8c9463a74184666f
bluurosebud <bluurosebud@gmail.com>
2018-02-12 22:13:20 -0500

    changed num divisible by 5 to minds

--- minedminds_kata.rb
+++ minedminds_kata.rb
@@ -1,10 +1,10 @@
 num=1
-
-100.times do
-	if num==5
-		puts "minds"
-	else
-		puts(num)
-	end
-	num+=1
-end
+  
+  100.times do
+ 	if (num % 5)===0
+  		puts "minds"
+  	else
+  		puts(num)
+ 	end
+ 	num+=1
+ end
