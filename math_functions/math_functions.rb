02bdc7b11e6261847da737d3986cd67c0807e2c9
bluurosebud <bluurosebud@gmail.com>
2018-02-13 14:11:59 -0500

    created crust function

--- math_functions/math_functions.rb
+++ math_functions/math_functions.rb
@@ -5,12 +5,19 @@ def subtracting(pink,yellow)
 	pink - yellow
 end
 def divide(pink,yellow)
-	pink / yellow
+	if yellow ==0
+		"does not work"
+		
+	else
+		return pink.to_f / yellow.to_f
+	end
 end
 def multiply(pink,yellow)
 	pink * yellow
 end
-p adding(1,4)
-p subtracting(10,6)
-p divide(20,5)
-p multiply(3,4)
+# p adding(1,4)
+# p subtracting(10,6)
+p divide(20,0)
+# p multiply(3,4)
+p divide(10,2)
+# p divide(5,9)
