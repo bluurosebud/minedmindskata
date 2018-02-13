c3b2f15025297db2f65f95921a733accc4d3ca33
bluurosebud <bluurosebud@gmail.com>
2018-02-13 09:30:18 -0500

    changed num that are divisible by 3 and 5 to mined minds

--- minedminds_kata.rb
+++ minedminds_kata.rb
@@ -1,14 +1,16 @@
 num=1
 
 100.times do                                                     
+	if (num % 15 == 0)
+		puts "mined minds"
+			
+    elsif (num % 3 == 0)                                                               
+        puts "mined"                                                            
+    elsif (num % 5 == 0)                                                            
+        puts "minds"        
+    else
+       	puts (num)
 
-        if (num % 3 == 0)                                                               
-                puts "mined"                                                            
-        elsif (num % 5 == 0)                                                            
-                puts "minds"
-        else
-        	puts (num)
-
-        end
-        num+=1                                                                        
+    end
+    num+=1                                                                        
 end           
